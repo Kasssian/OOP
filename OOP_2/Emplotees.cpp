471737810b68252f4e72ac1e445888101b1bf518
Acer <Acer@AMADEY>
2025-01-31 14:06:27 +0600

    Добавьте файлы проекта.

---
+++ OOP_2/Emplotees.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//using namespace std;
+//
+//struct employee {
+//	int id;
+//	float manual;
+//};
+//
+//int main() {
+//	employee employee1, employee2, employee3;
+//
+//	cout << "Enter id for first employee: " << endl;
+//	cin >> employee1.id;
+//	cout << "Enter manual for first employee: " << endl;
+//	cin >> employee1.manual;
+//
+//	cout << "Enter  id for second employee: " << endl;
+//	cin >> employee2.id;
+//	cout << "Enter manual for second employee: " << endl;
+//	cin >> employee2.manual;
+//
+//	cout << "Enter id for third employee: " << endl;
+//	cin >> employee3.id;
+//	cout << "Enter manual for third employee: " << endl;
+//	cin >> employee3.manual;
+//
+//	cout << "id first employee: " << employee1.id << " and his manual: " << employee1.manual << endl;
+//	cout << "id second employee: " << employee2.id << " and his manual: " << employee2.manual << endl;
+//	cout << "id third employee: " << employee3.id << " and his manual: " << employee3.manual << endl;
+//}
